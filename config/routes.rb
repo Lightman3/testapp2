d626c2015823940840a8c4930671190668a42b9f
Lightman3 <erick.alarcon7@gmail.com>
2019-01-05 17:48:20 -0300

    ajax load stock

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
-  # get 'users/my_portfolio'
+  get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
   root 'users#my_portfolio'
   # root 'welcome#index'

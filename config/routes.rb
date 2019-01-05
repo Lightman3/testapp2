7eb881f799c1e8ea1e2e36e43499dc24b0d0ff53
Lightman3 <erick.alarcon7@gmail.com>
2019-01-05 18:40:06 -0300

    user follow stock and add restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
-  root 'users#my_portfolio'
-  # root 'welcome#index'
+  # root 'users#my_portfolio'
+  root 'welcome#index'
+  resources :user_stocks, only: [:create]
   # get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
